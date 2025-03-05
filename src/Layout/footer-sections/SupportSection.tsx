4d68f97b7d10f978a610b114365bfb90e9a17c11
Cédric <cedricvanhoorebeke@outlook.com>
2025-03-05 16:21:35 +0100

    002.2-SupportSection

--- src/Layout/footer-sections/SupportSection.tsx
+++ src/Layout/footer-sections/SupportSection.tsx
@@ -1,6 +1,18 @@
 import style from "../../css/footer.module.css";
 
 const SupportSection = () => {
-  return <div>SupportSection</div>;
+  return (
+    <div className={style.SupportSection}>
+      <div className={style.Support}>Support</div>
+      <div className={style.Frame712}>
+        <div className={style.Address}>
+          111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.
+        </div>
+        <div className={style.Mail}>exclusive@gmail.com</div>
+        <div className={style.Phone}>+88015-88888-9999</div>
+      </div>
+    </div>
+  );
 };
+
 export default SupportSection;
