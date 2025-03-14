b9ad27522bad95a12511e620485a57569f9d597a
Cédric <cedricvanhoorebeke@outlook.com>
2025-03-14 22:57:26 +0100

    Layout-Section-Categories-component-006

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Layout from "./Layout/Layout";
+import { Categories } from "./components/categoriesComponent";
 import { FlashSales } from "./components/today.component";
+import "./css/App.css";
 
 const App = () => {
   return (
     <Layout>
       <FlashSales />
+      <hr />
+      <Categories />
+      <hr />
     </Layout>
   );
 };
---
+++ src/components/categoriesComponent.tsx
@@ -0,0 +1,63 @@
+import styles from "../css/Components-css/categoriesComponent.module.css";
+import {
+  FaMobileAlt,
+  FaDesktop,
+  FaApple,
+  FaCamera,
+  FaHeadphones,
+  FaGamepad,
+} from "react-icons/fa";
+
+const categories = [
+  { name: "Phones", icon: FaMobileAlt },
+  { name: "Computers", icon: FaDesktop },
+  { name: "SmartWatch", icon: FaApple },
+  { name: "Camera", icon: FaCamera },
+  { name: "HeadPhones", icon: FaHeadphones },
+  { name: "Gaming", icon: FaGamepad },
+];
+
+export function Categories() {
+  return (
+    <section className={styles.categories}>
+      <div className={styles.header}>
+        <div className={styles.title}>
+          <div className={styles.titleWrapper}>
+            <div className={styles.bar}></div>
+            <h3>Categories</h3>
+          </div>
+          <h2>Browse By Category</h2>
+        </div>
+
+        <div className={styles.navigation}>
+          <button className={styles.navButton} aria-label="Previous category">
+            <svg className={`${styles.icon} prev`} />
+          </button>
+          <button
+            className={`${styles.navButton} next`}
+            aria-label="Next category"
+          >
+            <svg className={`${styles.icon}`} />
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.categoryGrid}>
+        {categories.map((category, index) => {
+          const Icon = category.icon;
+          return (
+            <button
+              key={category.name}
+              className={`${styles.categoryButton} ${
+                index === 3 ? styles.active : ""
+              }`}
+            >
+              <Icon className={styles.categoryIcon} />
+              <span className={styles.categoryText}>{category.name}</span>
+            </button>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
