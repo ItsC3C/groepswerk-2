c8af84b0acce323919afd56a4cde128f064b4b9c
robin <robin1broos@gmail.com>
2025-03-20 21:11:18 +0100

    added localstoragee to cart

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -8,9 +8,18 @@ interface CartItem {
 interface CartState {
   items: CartItem[];
 }
+// load and save cart from local storage
+const loadCartFromLocalStorage = (): CartItem[] => {
+  const storedCart = localStorage.getItem("cart");
+  return storedCart ? JSON.parse(storedCart) : [];
+};
+
+const saveCartToLocalStorage = (cart: CartItem[]) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
 
 const initialState: CartState = {
-  items: [],
+  items: loadCartFromLocalStorage(),
 };
 
 const cartSlice = createSlice({
@@ -24,11 +33,25 @@ const cartSlice = createSlice({
       } else {
         state.items.push({ id: action.payload, quantity: 1 });
       }
+      saveCartToLocalStorage(state.items); // Save cart to local storage
     },
     removeFromCart: (state, action: PayloadAction<string>) => {
       state.items = state.items.filter((item) => item.id !== action.payload);
+      saveCartToLocalStorage(state.items); // Save cart to local storage
+    },
+    clearCart: (state) => {
+      state.items = [];
+      localStorage.removeItem("cart"); // clear cart from local storage
     },
   },
+
+  // 🔴 api logica voor later😊
+  // extraReducers: (builder) => {
+  //   builder.addCase(fetchCart.fulfilled, (state, action) => {
+  //     state.items = action.payload;
+  //     saveCartToLocalStorage(action.payload);
+  //   });
+  // },
 });
 
 export const { addToCart, removeFromCart } = cartSlice.actions;
