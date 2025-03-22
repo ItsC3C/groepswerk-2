9f9cc5cd36f551c1f0ee69b67cdae4d3b414dea8
robin <robin1broos@gmail.com>
2025-03-22 14:44:20 +0100

    sidebar carousel

--- src/components/HomeComponents/SidebarAndCarousel.tsx
+++ src/components/HomeComponents/SidebarAndCarousel.tsx
@@ -1,5 +1,5 @@
-import Apple from "../../assets/apple.png";
-import HeroImage from "../../assets/HeroImage.png";
+import { useEffect, useState } from "react";
+import PokeLogo from "../../assets/pokemon_vector.webp";
 import styles from "../../css/Components-css/HomeCSS/SidebarAndCarousel.module.css";
 
 export function SidebarAndCarousel() {
@@ -14,6 +14,43 @@ export function SidebarAndCarousel() {
     "Groceries & Pets",
     "Health & Beauty",
   ];
+
+  const carouselImages = [
+    "https://images.pokemontcg.io/ex12/23_hires.png",
+    "https://images.pokemontcg.io/dp3/25_hires.png",
+    "https://images.pokemontcg.io/sm115/23_hires.png",
+    "https://images.pokemontcg.io/ex3/21_hires.png",
+  ];
+  const [animate, setAnimate] = useState(false);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === carouselImages.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [carouselImages.length]);
+
+  useEffect(() => {
+    setAnimate(true);
+    const timeout = setTimeout(() => setAnimate(false), 600); // 600ms = duur van je fadeImage animatie
+    return () => clearTimeout(timeout);
+  }, [currentIndex]);
+
+  const handleNext = () => {
+    setCurrentIndex((prev) =>
+      prev === carouselImages.length - 1 ? 0 : prev + 1
+    );
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) =>
+      prev === 0 ? carouselImages.length - 1 : prev - 1
+    );
+  };
 
   return (
     <div className={styles.container}>
@@ -33,13 +70,15 @@ export function SidebarAndCarousel() {
           <div className={styles.textContainer}>
             <div className={styles.textWrapper}>
               <img
-                src={Apple}
-                alt="Apple"
+                src={PokeLogo}
+                alt="Pokemon Logo"
                 width="40"
-                height="49"
+                height="40"
                 className={styles.appleImage}
               />
-              <h1 className={styles.title}>iPhone 14 Series</h1>
+              <h1 className={styles.title}>
+                Check Our Rare Pokemon Collection
+              </h1>
               <p className={styles.discount}>Up to 10% off Voucher</p>
               <a href="#" className={styles.shopNow}>
                 <span>Shop Now</span>
@@ -48,13 +87,25 @@ export function SidebarAndCarousel() {
             </div>
           </div>
           <div className={styles.heroImageContainer}>
-            <img
-              src={HeroImage}
-              alt="iPhone 14"
-              width="496"
-              height="352"
-              className={styles.heroImage}
-            />
+            <a href="#">
+              <img
+                src={carouselImages[currentIndex]}
+                alt={`Slide ${currentIndex + 1}`}
+                width="46px"
+                height="32px"
+                className={`${styles.heroImage} ${
+                  animate ? styles.fadeImageAnimation : ""
+                }`}
+              />
+            </a>
+            <div className={styles.carouselControls}>
+              <button onClick={handlePrev} className={styles.carouselButton}>
+                &#8592;
+              </button>
+              <button onClick={handleNext} className={styles.carouselButton}>
+                &#8594;
+              </button>
+            </div>
           </div>
         </div>
       </div>
